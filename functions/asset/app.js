318b6ed38ee5c6fa7940e4bcb11f9b9a982b68d6
LynAs Sazzad <szlynas@gmail.com>
2017-08-12 10:46:47 -0700

    init

---
+++ functions/asset/app.js
@@ -0,0 +1,20 @@
+(function ($) {
+    console.log("test");
+    $( ".mybtn" ).click(function() {
+        $.ajax({
+            url: "/",
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({
+                from: "dhaka",
+                to: "faridpur"
+            }),
+            success: function (result) {
+                $(".result").html(result);
+            }, error: function (error) {
+                console.log(error);
+            }
+
+        });
+    });
+})(jQuery);
---
+++ functions/index.js
@@ -0,0 +1,13 @@
+const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
+const express = require('express');
+const app = express();
+const firebaseApp = firebase.initializeApp(functions.config().firebase);
+app.set('view engine', 'pug');
+app.use('/asset', express.static(__dirname + '/asset'));
+
+app.get('/', function(req, res) {
+    res.render('index', {title:"jogajog"});
+});
+
+exports.app= functions.https.onRequest(app);
