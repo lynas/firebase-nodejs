7facf36b728cb55757ccee9b0651ac051f887b42
LynAs Sazzad <szlynas@gmail.com>
2017-08-13 04:40:04 -0700

    templating

--- functions/index.js
+++ functions/index.js
@@ -6,8 +6,138 @@ const firebaseApp = firebase.initializeApp(functions.config().firebase);
 app.set('view engine', 'pug');
 app.use('/asset', express.static(__dirname + '/asset'));
 
-app.get('/', function(req, res) {
-    res.render('index', {title:"jogajog"});
+app.get('/', function (req, res) {
+    res.render('index', {title: "jogajog"});
 });
 
-exports.app= functions.https.onRequest(app);
+app.post('/', function (req, res) {
+    res.render('search_card', {
+        title: "jogajog", data: [
+            {
+                bus_name: "Ajmiri Enterprise",
+                counter: "Gabtoli",
+                route: "Dhaka to Faridpur",
+                contact: "964412542",
+                booking: "",
+                bus_types: [
+                    {
+                        type_name: "economy",
+                        ticket_cost: 350
+                    },
+                    {
+                        type_name: "ac",
+                        ticket_cost: 450
+                    },
+                    {
+                        type_name: "business",
+                        ticket_cost: 550
+                    }
+                ],
+                bus_schedules: [
+                    {
+                        days: "everyday",
+                        times: [
+                            {
+                                schedule_time: "9am",
+                                bus_type: "economy"
+                            },
+                            {
+                                schedule_time: "10am",
+                                bus_type: "economy"
+                            },
+                            {
+                                schedule_time: "11am",
+                                bus_type: "economy"
+                            },
+                            {
+                                schedule_time: "12am",
+                                bus_type: "ac"
+                            },
+                            {
+                                schedule_time: "9am",
+                                bus_type: "ac"
+                            },
+                            {
+                                schedule_time: "9am",
+                                bus_type: "economy"
+                            },
+                            {
+                                schedule_time: "9am",
+                                bus_type: "business"
+                            },
+                            {
+                                schedule_time: "9am",
+                                bus_type: "economy"
+                            },
+                            {
+                                schedule_time: "9am",
+                                bus_type: "economy"
+                            },
+                            {
+                                schedule_time: "9am",
+                                bus_type: "ac"
+                            },
+                            {
+                                schedule_time: "9am",
+                                bus_type: "ac"
+                            },
+                            {
+                                schedule_time: "9am",
+                                bus_type: "economy"
+                            },
+                            {
+                                schedule_time: "9am",
+                                bus_type: "business"
+                            },
+                            {
+                                schedule_time: "9am",
+                                bus_type: "economy"
+                            },
+                            {
+                                schedule_time: "9am",
+                                bus_type: "economy"
+                            },
+                            {
+                                schedule_time: "9am",
+                                bus_type: "economy"
+                            },
+                            {
+                                schedule_time: "9am",
+                                bus_type: "economy"
+                            },
+                            {
+                                schedule_time: "9am",
+                                bus_type: "economy"
+                            },
+                            {
+                                schedule_time: "9am",
+                                bus_type: "economy"
+                            }
+                        ]
+                    },
+                    {
+                        days: "Sunday",
+                        times: [
+                            {
+                                schedule_time: "9am",
+                                bus_type: "economy"
+                            },
+                            {
+                                schedule_time: "10am",
+                                bus_type: "economy"
+                            },
+                            {
+                                schedule_time: "11am",
+                                bus_type: "economy"
+                            }
+
+                        ]
+                    }
+                ]
+            }
+        ]
+    });
+});
+
+exports.app = functions.https.onRequest(app);
+
