28d8940e7b191f0baef33a9462d5f9c0c3cfd094
LynAs <szlynas@gmail.com>
2017-08-16 02:54:38 +0600

    input json done

--- public/app.js
+++ public/app.js
@@ -78,9 +78,131 @@
             });
         }
         input_json.bus_types = bus_types;
+        var every_day_time = [];
+        var saturday_day_time = [];
+        var sunday_day_time = [];
+        var monday_day_time = [];
+        var tuesday_day_time = [];
+        var wednesday_day_time = [];
+        var thursday_day_time = [];
+        var friday_day_time = [];
 
-        console.log(JSON.stringify(data));
+        for (var i = 1; i <= counter; i++) {
+            const schedule_day = "option_schedule_day_" + i;
+            const bus_type = "option_bus_type_" + i;
+            const bus_schedule = "bus_schedule_" + i;
+            const schedule_meridiem = "option_meridiem_" + i;
+            console.log(data[schedule_day]);
+            console.log(data[bus_type]);
+            console.log(data[bus_schedule]);
+            console.log(data[schedule_meridiem]);
+
+            if (data[schedule_day] == "everyday") {
+                every_day_time.push({
+                    schedule_time: data[bus_schedule] + data[schedule_meridiem],
+                    bus_type: data[bus_type]
+                });
+            } else if (data[schedule_day] == "saturday") {
+                saturday_day_time.push({
+                    schedule_time: data[bus_schedule] + data[schedule_meridiem],
+                    bus_type: data[bus_type]
+                });
+            } else if (data[schedule_day] == "sunday") {
+                sunday_day_time.push({
+                    schedule_time: data[bus_schedule] + data[schedule_meridiem],
+                    bus_type: data[bus_type]
+                });
+            } else if (data[schedule_day] == "monday") {
+                monday_day_time.push({
+                    schedule_time: data[bus_schedule] + data[schedule_meridiem],
+                    bus_type: data[bus_type]
+                });
+            } else if (data[schedule_day] == "tuesday") {
+                tuesday_day_time.push({
+                    schedule_time: data[bus_schedule] + data[schedule_meridiem],
+                    bus_type: data[bus_type]
+                });
+            } else if (data[schedule_day] == "wednesday") {
+                wednesday_day_time.push({
+                    schedule_time: data[bus_schedule] + data[schedule_meridiem],
+                    bus_type: data[bus_type]
+                });
+            } else if (data[schedule_day] == "thursday") {
+                thursday_day_time.push({
+                    schedule_time: data[bus_schedule] + data[schedule_meridiem],
+                    bus_type: data[bus_type]
+                });
+            } else if (data[schedule_day] == "friday") {
+                friday_day_time.push({
+                    schedule_time: data[bus_schedule] + data[schedule_meridiem],
+                    bus_type: data[bus_type]
+                });
+            }
+
+
+        }
+        var bus_schedules = [];
+        if (every_day_time.length !== 0) {
+            bus_schedules.push(
+                {
+                    days : "everyday",
+                    times : every_day_time
+                }
+            );
+        }
+        if (saturday_day_time.length !== 0) {
+            bus_schedules.push(
+                {
+                    days : "saturday",
+                    times : saturday_day_time
+                }
+            );
+        }
+        if (sunday_day_time.length !== 0) {
+            bus_schedules.push(
+                {
+                    days : "sunday",
+                    times : sunday_day_time
+                }
+            );
+        }
+        if (monday_day_time.length !== 0) {
+            bus_schedules.push(
+                {
+                    days : "monday",
+                    times : monday_day_time
+                }
+            );
+        }
+        if (tuesday_day_time.length !== 0) {
+            bus_schedules.push(
+                {
+                    days : "tuesday",
+                    times : tuesday_day_time
+                }
+            );
+        }
+        if (thursday_day_time.length !== 0) {
+            bus_schedules.push(
+                {
+                    days : "thursday",
+                    times : thursday_day_time
+                }
+            );
+        }
+        if (friday_day_time.length !== 0) {
+            bus_schedules.push(
+                {
+                    days : "friday",
+                    times : friday_day_time
+                }
+            );
+        }
+        input_json.bus_schedules  = bus_schedules;
+        // console.log(JSON.stringify(data));
+        console.log("--------OUT--------")
         console.log(JSON.stringify(input_json));
+        console.log("--------OUTEND--------")
         return false;
     });
 
@@ -88,9 +210,9 @@
         event.preventDefault();
         counter++;
         $(".bus_schedule").append('<br />' +
-            '<select name="option_bus_schedule_day_' + counter + '"><option>Everyday</option><option>Saturday</option><option>Sunday</option><option>Monday</option><option>Tuesday</option><option>Wednesday</option><option>Thursday</option><option>Friday</option></select>' +
-            '<select name="option_bus_schedule_' + counter + '"><option>economy</option><option>ac</option><option>business</option></select>' +
+            '<select name="option_schedule_day_' + counter + '"><option>everyday</option><option>saturday</option><option>sunday</option><option>monday</option><option>tuesday</option><option>wednesday</option><option>thursday</option><option>friday</option></select>' +
+            '<select name="option_bus_type_' + counter + '"><option>economy</option><option>ac</option><option>business</option></select>' +
             '<span style="margin-left:10px; margin-right:10px;"><input name="bus_schedule_' + counter + '"></span>' +
-            '<select name="option_time_bus_schedule_' + counter + '"><option>am</option><option>pm</option></select>');
+            '<select name="option_meridiem_' + counter + '"><option>am</option><option>pm</option></select>');
     });
 })(jQuery);
